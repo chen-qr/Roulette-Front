61b3f4b1ba003021ae756a345af0b86356450975
Alex <alex.chenqr@gmail.com>
2023-12-26 21:48:04 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ function index({}) {
                     })
                     .then((accounts) => accounts[0])
                     .catch(() => {
-                    // if the user cancels the login prompt
-                    throw Error("Please select an account");
+                        // if the user cancels the login prompt
+                        throw Error("Please select an account");
                     });
         
                 // set the global userWalletAddress variable to selected account
