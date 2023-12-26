33452fc7d3f1fb82157e366961a3f3ac723c88ac
Alex <alex.chenqr@gmail.com>
2023-12-26 16:35:23 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import styles from '../styles/index.module.css'
 import Web3 from 'web3'
 import { Wallet } from './interface'
+import React, { useState } from 'react'
 
 function index({}) {
+    const [count, setCount] = useState(0)
+    function increment() {
+        setCount(count + 1)
+    }
+
     return (
     <div>
-        <div>连接钱包</div>
-        <div>Address:</div>
+        <div>
+            <div>连接钱包</div>
+            <div>Address:</div>
+        </div>
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={increment}>Click me</button>
+        </div>
     </div>
     );
 }
