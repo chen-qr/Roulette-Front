b4311b5e27a380f498ef308e62220e1b1475bb45
Alex <alex.chenqr@gmail.com>
2023-12-26 21:46:14 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,35 @@ function index({}) {
     const [walletAddress, setWalletAddress] = useState("")
 
     useEffect(() => {
-        setWalletAddress(window.localStorage.getItem("userWalletAddress"))
+
+        async function name() {
+            if (window.web3) {
+                try {
+                // get the user's ethereum account - prompts metamask to login
+                const selectedAccount = await window.ethereum
+                    .request({
+                        method: "eth_requestAccounts",
+                    })
+                    .then((accounts) => accounts[0])
+                    .catch(() => {
+                    // if the user cancels the login prompt
+                    throw Error("Please select an account");
+                    });
+        
+                // set the global userWalletAddress variable to selected account
+                setWalletAddress(selectedAccount);
+        
+                // store the user's wallet address in local storage
+                window.localStorage.setItem("userWalletAddress", selectedAccount);
+        
+                } catch (error) {
+                alert(error);
+                }
+            } else {
+                alert("wallet not found");
+            }
+        }
+        name()
     })
 
     return (
