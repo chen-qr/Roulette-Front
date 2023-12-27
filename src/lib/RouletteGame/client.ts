054cf9bfffc48df343842403a9fc629b8672aed2
Alex <alex.chenqr@gmail.com>
2023-12-27 11:28:52 +0800

    update

--- src/lib/RouletteGame/client.ts
+++ src/lib/RouletteGame/client.ts
@@ -3,21 +3,23 @@ import Web3 from "web3";
 
 let rouletteGame = null;
 
-async function createContract(walletAddress: string) {
-    const web3 = new Web3(window.ethereum);
-    rouletteGame = new web3.eth.Contract(abi, contractAddress, {from: walletAddress});
+function getContract(walletAddress: string) {
+    if (rouletteGame == undefined || rouletteGame == null) {
+        const web3 = new Web3(window.ethereum);
+        rouletteGame = new web3.eth.Contract(abi, contractAddress, {from: walletAddress});
+    }
+    return rouletteGame;
 }
 
 export async function initPlayerScore(walletAddress: string, setPlayerScore: Function) {
-    if (rouletteGame == undefined || rouletteGame == null) {
-    createContract(walletAddress)
-}  
-
-const score = await rouletteGame.methods.getScore().call();
-    setPlayerScore(parseInt(score))
+    const rouletteGame = getContract(walletAddress);
+    const score = await rouletteGame.methods.getScore().call();
+        setPlayerScore(parseInt(score))
 }
 
 export async function deposit(walletAddress: string, amount: number) {
+    const rouletteGame = getContract(walletAddress);
+
     console.log("deposit", walletAddress, amount);
     if (amount < 100000000) {
         const errMsg = "deposit amount can not less than 100000000!";
