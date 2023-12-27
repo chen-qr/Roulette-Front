d4a09e0d4f14de6f928c6e10a03bd3b6a71c621f
Alex <alex.chenqr@gmail.com>
2023-12-27 23:52:05 +0800

    update

--- src/lib/RouletteGame/client.ts
+++ src/lib/RouletteGame/client.ts
@@ -1,14 +1,11 @@
 import {contractAddress, abi} from './config'
+import Web3 from "web3";
 
 let rouletteGame = null;
 
 function getContract(walletAddress: string) {
     if (rouletteGame == undefined || rouletteGame == null) {
         console.log("Initializing contract");
-        // const web3 = new Web3(window.ethereum);
-        // rouletteGame = new web3.eth.Contract(abi, contractAddress, {from: walletAddress});
-        // 下面是浏览器的代码，从浏览器获取库
-        const { Web3 } = require('web3');
         const web3 = new Web3(window.ethereum);
         rouletteGame = new web3.eth.Contract(abi, contractAddress, {from: walletAddress});
     }
@@ -34,6 +31,6 @@ export async function deposit(walletAddress: string, amount: number) {
     // https://github.com/web3/web3.js/issues/4258
     await rouletteGame.methods
         .deposit()
-        // .send({from: walletAddress, value: amount});
-        .send({from: walletAddress, value: amount.toString, type: "0x0", method: 'personal_sign'});
+        .send({from: walletAddress, value: amount});
+        // .send({from: walletAddress, value: amount.toString, type: "0x0", method: 'personal_sign'});
 };
