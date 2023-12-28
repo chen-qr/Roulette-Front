b01645014af74a7002b04fe421e446c2e8055fe8
Alex <alex.chenqr@gmail.com>
2023-12-28 16:34:45 +0800

    update

--- src/components/BetArea/Layout.tsx
+++ src/components/BetArea/Layout.tsx
@@ -5,21 +5,40 @@ import BetNumberArea from '../BetNumberArea/Layout'
 const BetArea = () => {
     const beginNum = 1;
     const endNum = 36;
-    const nums = Array.from({length: endNum - beginNum + 1}, (_, i) => i + beginNum);
     const lineCnt = 3;
     if ((endNum - beginNum + 1) % lineCnt !== 0){
         throw new Error('endNum - beginNum + 1 must be a multiple of lineCnt');
     }
     const colCnt = (endNum - beginNum + 1) / lineCnt;
+    const colNums = Array.from({length: colCnt - beginNum + 1}, (_, i) => i + beginNum);
+    const lineNums = Array.from({length: lineCnt}, (_, i) => i + beginNum);
 
-    const renderLines = () => {
-        
+    // 渲染列元素
+    const renderCols = (iLine: number) => {
+        // 生成列元素
+        const colItems = colNums.map((iCol) => {
+            const number = iCol + (iLine - 1) * colCnt;
+            const color = (number + iLine) % 2 === 0 ? "red" : "black"
+            return <BetNumberArea key={iCol}  color={color} number={number} />;
+        });
+        return (
+            <div className={styles.colLayout}>{colItems}</div>
+        );
+    }
+    // 渲染行元素
+    const renderRows = () => {
+        // 生成行元素
+        const rowItems = lineNums.map((iLine) => {
+            return <div key={iLine} >{renderCols(iLine)}</div>;
+        });
+        return (
+            <div>{rowItems}</div>
+        );
     }
 
     return (
         <div>
-            <BetNumberArea color={"red"} number={1}/>
-            <BetNumberArea color={"black"} number={2}/>
+            {renderRows()}
         </div>
     );    
 }
