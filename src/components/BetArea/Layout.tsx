03e33e5ef861200c6152b748e1eddcc188124766
Alex <alex.chenqr@gmail.com>
2023-12-28 16:00:23 +0800

    update

--- src/components/BetArea/Layout.tsx
+++ src/components/BetArea/Layout.tsx
@@ -3,6 +3,19 @@ import styles from './Layout.module.css'
 import BetNumberArea from '../BetNumberArea/Layout'
 
 const BetArea = () => {
+    const beginNum = 1;
+    const endNum = 36;
+    const nums = Array.from({length: endNum - beginNum + 1}, (_, i) => i + beginNum);
+    const lineCnt = 3;
+    if ((endNum - beginNum + 1) % lineCnt !== 0){
+        throw new Error('endNum - beginNum + 1 must be a multiple of lineCnt');
+    }
+    const colCnt = (endNum - beginNum + 1) / lineCnt;
+
+    const renderLines = () => {
+        
+    }
+
     return (
         <div>
             <BetNumberArea color={"red"} number={1}/>
