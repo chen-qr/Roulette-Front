3dd3c408d64055251aadaa9a05ee52147cda45cf
Alex <alex.chenqr@gmail.com>
2023-12-28 19:37:22 +0800

    update

--- src/components/PlayerScore/Layout.tsx
+++ src/components/PlayerScore/Layout.tsx
@@ -5,16 +5,23 @@ import styles from './Layout.module.css'
 const PlayerScore = ({playerScore}) => {
 
     let playerScoreText = "";
+    let uintText = ""
     if (playerScore <= 0) {
         playerScoreText = "waiting..."
+        uintText = ""
     } else {
         playerScoreText = playerScore.toLocaleString()
+        let ethAmount = playerScore / 1000000000000000000
+        uintText = `wei = ${ethAmount} eth`
     }
 
     return (
         <div className={styles.scoreBoard}>
             <div className={styles.balance}>Your balance</div>
-            <div className={styles.scoreNumber}>{playerScoreText}</div>
+            <div className={styles.balanceContent}>
+                <div className={styles.scoreNumber}>{playerScoreText}</div>
+                <div className={styles.scoreUint}>{uintText}</div>
+            </div>
         </div>
     );    
     
