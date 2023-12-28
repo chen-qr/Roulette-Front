45090fce3712fb6174a0bcb04da5e9ce79b65ed3
Alex <alex.chenqr@gmail.com>
2023-12-28 17:28:56 +0800

    update

--- src/components/WalletConnect/Layout.tsx
+++ src/components/WalletConnect/Layout.tsx
@@ -3,16 +3,20 @@ import styles from './Layout.module.css'
 
 
 const WalletConnect = ({walletAddress}) => {
+
+    const isActive = walletAddress != undefined && walletAddress != "" ? true : false
     
     const [logSuggest, setLogSuggest] = useState("Please connect your wallet!")
     useEffect(() => { 
-        if (walletAddress != undefined && walletAddress != "") { setLogSuggest("Wallet connected") }
+        if (isActive) { setLogSuggest("Wallet connected") }
     }, [walletAddress])
+    
+    const getActiveColor = () => { return isActive ? styles.logSuggestActive : styles.logSuggestInActive}
 
     return (
         <div>
             <div>
-                <div className={styles.logSuggest}>{logSuggest}</div>
+                <div className={`${styles.logSuggest} ${getActiveColor()}`}>{logSuggest}</div>
                 <div>Address: {walletAddress}</div>
             </div>
         </div>
