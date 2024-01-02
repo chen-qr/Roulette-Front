1b6efedf3eb43e11fe088dce4d0b71c3462de8d8
Alex <alex.chenqr@gmail.com>
2024-01-02 16:19:22 +0800

    update

--- deploy_server.py
+++ deploy_server.py
@@ -1,18 +1,65 @@
-import subprocess
+import paramiko
+from scp import SCPClient
 import os
+import time
+import subprocess
+
+export_dir = ".product"
+zip_name = "product.zip"
+remote_dir = "/root/lightlink_roulette"
 
 # 检查 .product 文件夹是否存在，如果存在则删除
-if os.path.exists(".product"):
-    subprocess.getoutput("rm -rf .product")
+if os.path.exists(export_dir):
+    subprocess.getoutput(f"rm -rf {export_dir}")
     
 # 检查 .product 文件夹是否存在
-if not os.path.exists(".product"):
-    os.makedirs(".product")
+if not os.path.exists(export_dir):
+    os.makedirs(export_dir)
 
 # 克隆代码到本地
-cp_product_fils = f"cp -rf .next package.json .product"
-subprocess.getoutput(cp_product_fils)
+cp_product_fils = f"cp -rf .next package.json {export_dir}"
+print(subprocess.getoutput(cp_product_fils))
 
-# 发布代码到服务器
+# 打包文件
+# zip打包默认不包含隐藏文件夹，所以要特意说明打包 .next
+cmd_zip = f"cd {os.path.join(os.getcwd(), export_dir)} && rm -f {zip_name} && zip -r {zip_name} * .next"
+print(subprocess.getoutput(cmd_zip))
 
-# 在服务器编译
+local_folder = os.path.join(os.getcwd(), export_dir, zip_name)
+
+# 上传到服务器
+key = paramiko.RSAKey.from_private_key_file("/Users/chenqirong/.ssh/id_rsa", password="chenqirong")
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('207.246.66.68', username='root', pkey=key)
+stdin, stdout, stderr = ssh.exec_command(f"rm -rf {remote_dir}/*")
+print("已经清空服务器上的文件夹")
+
+start_time = time.time()
+try:
+    scpclient = SCPClient(ssh.get_transport(), socket_timeout=35.0)
+    scpclient.put(local_folder, remote_dir, True)
+except Exception as e:
+    print("上传失败")
+    print(str(e.args))
+    raise Exception("上传失败！！！")
+else:
+    print("上传成功")
+finally:
+    scpclient.close()
+    end_time = time.time()
+    print("上传耗时: {:.2f}秒".format(end_time - start_time))
+
+try:
+    stdin, stdout, stderr = ssh.exec_command(f"cd {remote_dir} && unzip {zip_name} && rm {zip_name}")
+    if stdout.channel.recv_exit_status() != 0:
+        print("解压失败")
+except Exception as e:
+    print("解压失败")
+    print(str(e.args))
+else:
+    print("解压成功")
+
+# 关闭连接
+ssh.close()
