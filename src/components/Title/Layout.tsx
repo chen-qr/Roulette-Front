c3aadb629da3a4937a6ffb4b4620c093fa045f43
Alex <alex.chenqr@gmail.com>
2024-01-07 13:45:12 +0800

    update

---
+++ src/components/Title/Layout.tsx
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <div style={{display: "flex", margin: "1rem 5% 1rem 5% ", fontSize: "1.5rem"}}>
+            <div style={{color: "blue"}}>LightLink GameFi</div>
+        </div>
+    );
+}
+
+export default Title;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Title from '../components/Title/Layout'
+
+function index({}) {
+    return (
+        <div>
+            <Title />
+        </div>
+    );
+};
+
+export default index
