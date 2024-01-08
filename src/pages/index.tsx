44f94d30ceff6df0968666a0f8a06ad94cd23d92
Alex <alex.chenqr@gmail.com>
2024-01-08 18:13:31 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,56 @@
-import Title from '../components/Title/Layout'
+import React, { useState, useEffect } from 'react'
+import styles from './index.module.css'
+import {initWalletAddress} from '../lib/Wallet'
+import {initPlayerScore, deposit, withdraw, betAction, drawingAction} from '../lib/RouletteGame/client'
+import WalletConnect from '../components/WalletConnect/Layout'
+import PlayerScore from '../components/PlayerScore/Layout'
+import BetArea from '../components/BetArea/Layout'
 
 function index({}) {
+    const [walletAddress, setWalletAddress] = useState("")
+    useEffect(() => { initWalletAddress(setWalletAddress) })
+
+    const [playerScore, setPlayerScore] = useState(0)
+    useEffect(() => { 
+        if (walletAddress != undefined && walletAddress != "") { initPlayerScore(walletAddress, setPlayerScore) }
+    }, [walletAddress])
+    const onPlayerScoreChange = () => { initPlayerScore(walletAddress, setPlayerScore) }
+
+    // deposit click
+    const handleDepositClick = (depositAmount) =>{
+        deposit(walletAddress, depositAmount)
+        onPlayerScoreChange()
+    };
+    // withdraw click
+    const handleWithdrawClick = (withdrawAmount) =>{
+        withdraw(walletAddress, withdrawAmount, playerScore)
+        onPlayerScoreChange()
+    };
+    // bet click
+    const handleOnBetAction = (selectedNumber, betAmount, handleOnBetFinish) =>{
+        betAction(walletAddress, selectedNumber, betAmount, playerScore, handleOnBetFinish)
+    };
+    // drawing click
+    const handleDrawingClick = (selectedNumber, betAmount, userRandomNumber, sequenceNumber, handleOnDrawingFinish) =>{
+        drawingAction(walletAddress, selectedNumber, betAmount, userRandomNumber, sequenceNumber, handleOnDrawingFinish)
+    };
+
+    const handleSaveBetResult = (selectedNumber, betAmount, userRandomNumber, commitment, sequenceNumber, providerRandom, finalRandomNumber, drawNumber, isWin) => {
+        onPlayerScoreChange()
+    }
+
     return (
-        <div>
-            <Title />
-        </div>
+    <div className={styles.main}>
+        <div className={styles.title}>LightLink APAC Hackthoon - Chanllenge 1</div>
+        <WalletConnect walletAddress={walletAddress}/>
+        <PlayerScore playerScore={playerScore} onHandleDeposit={handleDepositClick} onHandleWithdraw={handleWithdrawClick}/>
+        <BetArea beginNum={1} endNum={36} lineCnt={3} playerScore={playerScore}
+            onBetAction={handleOnBetAction}
+            onDwaringAction={handleDrawingClick}
+            handleSaveBetResult={handleSaveBetResult}
+            />
+    </div>
     );
-};
+}
 
 export default index
