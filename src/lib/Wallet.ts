a6954b3fed48e7de3cc68f9e1a7ab03c7c7734fb
Alex <alex.chenqr@gmail.com>
2024-04-12 23:10:35 +0800

    add

---
+++ src/lib/Wallet.ts
@@ -0,0 +1,22 @@
+// TODO 帮助用户自动选择钱包网络
+export async function initWalletAddress(setWalletAddress: Function) {
+    if (window.web3) {
+        try {
+            const selectedAccount = await window.ethereum
+                .request({
+                    method: "eth_requestAccounts",
+                })
+                .then((accounts) => accounts[0])
+                .catch(() => {
+                    // if the user cancels the login prompt
+                    throw Error("Please select an account");
+                });
+
+            setWalletAddress(selectedAccount);
+        } catch (error) {
+            alert(error);
+        }
+    } else {
+        alert("wallet not found");
+    }
+};
